26eacbdb09f684d7c1edf0b95527e6cc23b66ccc
Ian Baron <ian.baron@manchester.ac.uk>
2024-01-03 16:26:00 +0000

    replace non-aplha-numeric characters

--- background.js
+++ background.js
@@ -6,7 +6,9 @@ browser.contextMenus.create({
 
 browser.contextMenus.onClicked.addListener((info, tab) => {
   if (info.menuItemId === "convert_to_snake_case") {
-    const convertedText = info.selectionText.replace(/([a-z])([A-Z])/g, '$1_$2').toLowerCase();
+    let convertedText = info.selectionText.replace(/([a-z])([A-Z])/g, '$1_$2').replace(/\s+/g, '_').toLowerCase();
+    // remove non-aplha-numeric characters
+    convertedText = convertedText.replace(/[^a-zA-Z0-9_](?=_)/g, '').replace(/,/g, '_');
     browser.tabs.sendMessage(tab.id, { action: "replaceText", convertedText });
   }
 });

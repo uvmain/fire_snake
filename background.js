7bd214f9d59a1edb2d26a1d8b5ee4ac7ee99552e
Ian Baron <ian.baron@manchester.ac.uk>
2024-01-04 11:57:26 +0000

    now works for DOM text and input text

--- background.js
+++ background.js
@@ -9,6 +9,10 @@ browser.contextMenus.onClicked.addListener((info, tab) => {
     let convertedText = info.selectionText.replace(/([a-z])([A-Z])/g, '$1_$2').replace(/\s+/g, '_').toLowerCase();
     // remove non-aplha-numeric characters
     convertedText = convertedText.replace(/[^a-zA-Z0-9_](?=_)/g, '').replace(/,/g, '_');
+    // remove leading and trailing underscores
+    convertedText = convertedText.replace(/^_/, '').replace(/_$/, '');
+    // remove multiple underscores
+    convertedText = convertedText.replace(/_+/g, '_');
     browser.tabs.sendMessage(tab.id, { action: "replaceText", convertedText });
   }
 });
--- content.js
+++ content.js
@@ -1,8 +1,23 @@
 function replaceSelectedText(message) {
-  const selection = window.getSelection();
-  const range = selection.getRangeAt(0);
-  range.deleteContents();
-  range.insertNode(document.createTextNode(message.convertedText));
+  let selection = window.getSelection();
+  if (selection.rangeCount > 0) {
+    const range = selection.getRangeAt(0);
+    range.deleteContents();
+    const textNode = document.createTextNode(message.convertedText);
+    range.insertNode(textNode);
+  }
+  else {
+    // if the selected text is in an input, the range will be less than one so we need to
+    const input = document.activeElement;
+    console.log(document.activeElement)
+    if (input && ['textarea', 'input'].includes(input.tagName.toLowerCase())) {
+      input.value = message.convertedText; 
+    }
+    else {
+      console.log('Input not found')
+    }
+  }
+
 }
 
 browser.runtime.onMessage.addListener((message, sender, sendResponse) => {

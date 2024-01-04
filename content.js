de162b97561c945ecb3d5422977dac4de2fa675e
Ian Baron <ian.baron@manchester.ac.uk>
2024-01-04 12:08:23 +0000

    try for input/text area first

--- content.js
+++ content.js
@@ -1,23 +1,22 @@
 function replaceSelectedText(message) {
-  let selection = window.getSelection();
-  if (selection.rangeCount > 0) {
-    const range = selection.getRangeAt(0);
-    range.deleteContents();
-    const textNode = document.createTextNode(message.convertedText);
-    range.insertNode(textNode);
+  
+  const input = document.activeElement;
+  if (input && ['textarea', 'input'].includes(input.tagName.toLowerCase())) {
+    input.value = message.convertedText; 
   }
   else {
-    // if the selected text is in an input, the range will be less than one so we need to
-    const input = document.activeElement;
-    console.log(document.activeElement)
-    if (input && ['textarea', 'input'].includes(input.tagName.toLowerCase())) {
-      input.value = message.convertedText; 
+    let selection = window.getSelection();
+    if (selection.rangeCount > 0) {
+      const range = selection.getRangeAt(0);
+      range.deleteContents();
+      const textNode = document.createTextNode(message.convertedText);
+      range.insertNode(textNode);
+      console.log('replacing text')
     }
     else {
       console.log('Input not found')
     }
   }
-
 }
 
 browser.runtime.onMessage.addListener((message, sender, sendResponse) => {

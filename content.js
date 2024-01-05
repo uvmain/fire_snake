f41f4ab97e54acdc9088c8cda61b52fcad8687f0
Ian Baron <ian.baron@manchester.ac.uk>
2024-01-05 09:11:45 +0000

    Dispatch an input event to simulate user input

--- content.js
+++ content.js
@@ -1,20 +1,24 @@
 function replaceSelectedText(message) {
-  
   const input = document.activeElement;
+
   if (input && ['textarea', 'input'].includes(input.tagName.toLowerCase())) {
-    input.value = message.convertedText; 
-  }
-  else {
+    input.value = message.convertedText;
+
+    const event = new Event('input', { bubbles: true });
+    input.dispatchEvent(event);
+  } else {
     let selection = window.getSelection();
+    
     if (selection.rangeCount > 0) {
       const range = selection.getRangeAt(0);
       range.deleteContents();
       const textNode = document.createTextNode(message.convertedText);
       range.insertNode(textNode);
-      console.log('replacing text')
-    }
-    else {
-      console.log('Input not found')
+
+      const event = new Event('input', { bubbles: true });
+      textNode.parentElement.dispatchEvent(event);
+    } else {
+      console.log('Input not found');
     }
   }
 }
